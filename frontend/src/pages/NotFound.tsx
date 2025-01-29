bd0a816c726af0323b6349060b457e91e982a70f
Victoria Lundberg <victoria.lundbrg@gmail.com>
2025-01-29 17:36:57 +0100

    💄 Not found page

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,3 +1,10 @@
+import { StartContainer } from "../styles/styledComponents/Containers";
+import { Heading } from "../styles/styledComponents/Items";
+
 export const NotFound = () => {
-  return <>NotFound</>;
+    return (
+        <StartContainer>
+            <Heading>Nothing here to see!</Heading>
+        </StartContainer>
+    );
 };
